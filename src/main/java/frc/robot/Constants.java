69936ea26f1c8d71c89924875d7a720b9becde97
Jeff Tuttle <jefft138@gmail.com>
2025-02-01 12:48:30 -0500

    Initial Elevator code based on 2019 Deep Space elevator

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class Constants {
+    public class ElevatorConstants {
+
+        public static final int LEFT_CANID = 1;
+        public static final int RIGHT_CANID = 2;
+
+    }
+}
