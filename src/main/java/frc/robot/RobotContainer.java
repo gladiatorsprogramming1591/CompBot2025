b9c3a1580c67f806dd01480f882cbe6ae01753c5
2025 DriverStation B <gladiatorsprogramming1591@gmail.com>
2025-03-15 10:44:16 -0400

    Slight optimization of L4 hover and auto
    Added constraint zone to first path, sped up L4 hover, and set end effector speed to 0 on teleop init.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -290,7 +290,7 @@ public class RobotContainer {
             : new ElevatorToPosition(elevator,position)
             .andThen(new WaitUntilCommand(elevator::atSetpoint))
             .andThen(wrist.L4HoverPositionCommand())
-            .andThen(new RunCommand(() -> endEffector.setCoralSpeed(-0.15),endEffector).withTimeout(0.25))
+            .andThen(new RunCommand(() -> endEffector.setCoralSpeed(-0.25),endEffector).withTimeout(0.15))
             .andThen(new InstantCommand(() -> endEffector.setCoralSpeed(0.0),endEffector))
             .andThen(new InstantCommand(() -> prepL4Finished(false)));
         }
