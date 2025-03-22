98f3430b022270d9d58b3ffc50e0990fbcc7cf8e
2025 DriverStation A <gladiatorsprogramming1591@gmail.com>
2025-03-22 18:34:36 -0400

    Minor tweaks to previous commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -189,8 +189,8 @@ public class RobotContainer {
         // Wrist
         operatorController.x().onTrue(new InstantCommand(() -> wrist.setAngle(WristConstants.GROUND_INTAKE)));
         operatorController.y().onTrue(new InstantCommand(() -> wrist.setAngle(WristConstants.WRIST_STOW)));
-        // operatorController.leftTrigger().onTrue(complexLowAlgaeIntakeCommand(elevatorPositions.ALGAE_LOW));
-        // operatorController.rightTrigger().onTrue(complexHighAlgaeIntakeCommand(elevatorPositions.ALGAE_HIGH));
+        operatorController.leftTrigger().onTrue(complexLowAlgaeIntakeCommand(elevatorPositions.ALGAE_LOW));
+        operatorController.rightTrigger().onTrue(complexHighAlgaeIntakeCommand(elevatorPositions.ALGAE_HIGH));
 
         operatorController.start().onTrue(new InstantCommand(() ->endEffector.setCoralSpeed(-1.0), endEffector))
                 .onFalse(new InstantCommand(() -> endEffector.setCoralSpeed(1.0), endEffector));
@@ -200,14 +200,12 @@ public class RobotContainer {
 
         operatorController.a().toggleOnTrue(new RunCommand(()->  flapServo.setFlapServoAngle(ServoConstants.kServoUpAngle))
                 .handleInterrupt(()-> flapServo.setFlapServoAngle(ServoConstants.kServoDownAngle)));
-        // climber.setDefaultCommand(climber.manualClimbMovement(
-        //         () -> MathUtil.applyDeadband(operatorController.getRightY(), STATIC_DEADBAND),
-        //         () -> MathUtil.applyDeadband(operatorController.getLeftY(), STATIC_DEADBAND)));
        
-        wrist.setDefaultCommand(new RunCommand(()-> wrist.setWristMotor(operatorController.getRightTriggerAxis() - operatorController.getLeftTriggerAxis() * 0.20), wrist)
-                .handleInterrupt(()-> wrist.setWristMotor(0)));
-        operatorController.rightStick().toggleOnTrue(new RunCommand(() -> elevator.setMotorSpeed(operatorController.getRightY() * 0.50), elevator)
-                .handleInterrupt(() -> elevator.setMotorSpeed(0)));
+        // Manual Control
+        // wrist.setDefaultCommand(new RunCommand(()-> wrist.setWristMotor(operatorController.getRightTriggerAxis() - operatorController.getLeftTriggerAxis() * 0.20), wrist)
+        //         .handleInterrupt(()-> wrist.setWristMotor(0)));
+        // operatorController.rightStick().toggleOnTrue(new RunCommand(() -> elevator.setMotorSpeed(operatorController.getRightY() * 0.50), elevator)
+        //         .handleInterrupt(() -> elevator.setMotorSpeed(0)));
 
         drivetrain.registerTelemetry(logger::telemeterize);
 
